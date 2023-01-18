749039356bc30e5420e68e676d83b5562b0df894
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-01-18 21:24:44 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -13,7 +13,7 @@ def private_use(func):
 
         if IS_PRIVATE and chat_id not in ADMINS:
             await message.reply_text(
-                "This bot only works for Admins. Make your own [Bot](https://github.com/kevinnadar22/URL-Shortener-V2)",
+                "This bot only works for Admins. [Contact Admin] (https://t.me/cyniteofficial)",
                 quote=True,
                 disable_web_page_preview=True,
             )
