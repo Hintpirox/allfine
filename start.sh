0e158e2dcb124e86582ca19fff3f8e3a23ae7392
Hintpirox <120027363+Hintpirox@users.noreply.github.com>
2023-01-20 16:03:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
-if [ -z $SOURCE_CODE ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Hintpirox/allfine.git /allfine
-else
-  echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /allfine
-fi
+echo "Cloning Repository"
+git clone https://github.com/Hintpirox/allfine.git /allfine
 cd /allfine
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
